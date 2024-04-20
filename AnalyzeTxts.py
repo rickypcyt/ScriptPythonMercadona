da041669092afcbd5d6d7bcd03990f3b1df72a4c
LeandroCabezass <Leocabezas296@gmail.com>
2024-04-20 13:27:04 -0500

    Finished explaining the entire call

--- AnalyzeTxts.py
+++ AnalyzeTxts.py
@@ -56,26 +56,30 @@ datos_por_ano_mes = defaultdict(lambda: defaultdict(list))
 
 # Iterar sobre los archivos en el directorio
 for archivo_name in os.listdir(directorio_entrada):
+    #iterates through the files that end with .txt
     if archivo_name.endswith(".txt"):
+        #Combine the directory path and the file name into a pull path to the file os.path.join()
         archivo_path = os.path.join(directorio_entrada, archivo_name)
         with open(archivo_path, "r", encoding="utf-8") as archivo:
+            #Reads the entire content of the file into the variable "texto_factura"
             texto_factura = archivo.read()
+            #Calls function extraer with the text of the invoice, returns list of tuples with item data and tot amount
             datos_factura, total_factura = extraer_datos_factura(texto_factura)
-            fecha = archivo_name.split()[0]
+            fecha = archivo_name.split()[0]  # It takes the first part as the date
             ano = fecha[:4]  # Extraer el año
             mes = fecha[4:6]  # Extraer el mes
-            nombre_mes = calendar.month_name[int(mes)]
+            nombre_mes = calendar.month_name[int(mes)]  # Convert the month num to an integer and then to its month
+            #Extends the list of data for the specific year and month with new invoice data, appends tuple and total amount
             datos_por_ano_mes[ano][nombre_mes].extend(datos_factura)
             datos_por_ano_mes[ano][nombre_mes].append(("TOTAL", "", total_factura))
 
+
 # Guardar los datos en archivos de texto en el directorio de salida
-for ano, datos_por_mes in datos_por_ano_mes.items():
-    for mes, datos_mes in datos_por_mes.items():
-        tabla_mes = tabulate(
-            datos_mes, headers=["Item", "Cantidad", "Total"], tablefmt="pretty"
-        )
-        tabla_mes = f"\nMes: {mes} | Año: {ano}\n" + tabla_mes
-        nombre_archivo = f"datos_facturas_{ano}_{mes}.txt"
-        ruta_archivo = os.path.join(directorio_salida, nombre_archivo)
-        with open(ruta_archivo, "w", encoding="utf-8") as archivo_salida:
-            archivo_salida.write(tabla_mes)
+for ano, datos_por_mes in datos_por_ano_mes.items():  # Each year
+    for mes, datos_mes in datos_por_mes.items():  # For each year, loops through each month
+        tabla_mes = tabulate(datos_mes, headers=["Item", "Cantidad", "Total"], tablefmt="pretty")
+        tabla_mes = f"\nMes: {mes} | Año: {ano}\n" + tabla_mes  # Uses the tabulate library to format the data into table
+        nombre_archivo = f"datos_facturas_{ano}_{mes}.txt"  # Constructs a filename for the output file based on yr and mo
+        ruta_archivo = os.path.join(directorio_salida, nombre_archivo)  # Joins the output directory path and filename
+        with open(ruta_archivo, "w", encoding="utf-8") as archivo_salida:  # Opens a file for writing
+            archivo_salida.write(tabla_mes)  # writes formatted table to the output file
